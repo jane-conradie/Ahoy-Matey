bae52085955bd5350541bff8c63a4e2c2b933e21
jane_conradie <jconradie1998@gmail.com>
2024-08-08 14:09:53 +0200

    implementing player shooting

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,7 +16,11 @@ public class Player : MonoBehaviour
     Vector2 minBounds;
     Vector2 maxBounds;
 
-    
+    Shooter shooter;
+
+    void Awake() {
+        shooter = GetComponent<Shooter>();
+    }
 
     void Start()
     {
@@ -51,5 +55,11 @@ public class Player : MonoBehaviour
         rawInput = value.Get<Vector2>();
     }
 
-
+    void OnFire(InputValue value)
+    {
+        if (shooter != null)
+        {
+            shooter.isFiring = value.isPressed;
+        }
+    }
 }
